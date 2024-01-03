278dfdc34fdfb6e5206ece9524f05e3ea0753a5e
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-01-03 22:08:04 +0530

    made changes to project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,7 @@ import Particle from "../Particle";
 import study from "../../Assets/Projects/StudyNotion login page.png"
 import currency from "../../Assets/Projects/currency.png"
 import weather from "../../Assets/Projects/weather.png"
-
+import ytss from "../../Assets/Projects/Yt(ss).png"
 
 function Projects() {
   return (
@@ -20,7 +20,7 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+          <Col md={9} lg={5} className="project-card">
             <ProjectCard
               imgPath={study}
              
@@ -33,7 +33,7 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={9} lg={5} className="project-card">
             <ProjectCard
               imgPath={currency}
               
@@ -45,7 +45,21 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={9} lg={5} className="project-card">
+            <ProjectCard
+              imgPath={ytss}
+
+              title="YouTube-Lite"
+              description="Integrated cost-effective free YouTube API with usage limits, if the DATA is not showing that means API reached usage limits."
+              description2="Developed a YouTube clone using React.js and integrated the YouTube API for real-time data."
+              description3="Implemented key features including category sections (Home, Trending, Music, etc.), robust search
+              functionality and a video player utilizing React Player."
+              ghLink="https://github.com/saqlain-shahid/youtube-lite.git"
+              demoLink="https://saqlain-youtube-lite.vercel.app"
+            />
+          </Col>
+
+          <Col md={9} lg={5} className="project-card">
             <ProjectCard
               imgPath={weather}
             
@@ -57,16 +71,7 @@ function Projects() {
             />
           </Col>
 
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={}
-              isBlog={false}
-              title=""
-              description=""
-              ghLink="https://github.com/"
-              demoLink="https://"
-            />
-          </Col> */}
+         
 
           {/* <Col md={4} className="project-card">
             <ProjectCard
